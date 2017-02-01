e11a0b124aff8c73fd8b4e814f97a77af754f22c
Sergii Grebeniuk <sergii@uberall.com>
2017-02-01 14:42:09 +0100

    add webpack config and a trivial react app

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const root = document.getElementById('root');
+
+ReactDOM.render(
+  <div>
+    React
+  </div>,
+  root
+);
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+const config = {
+    context: path.join(__dirname, "src"),
+    entry: "./app",
+    output: {
+        path: path.join(__dirname, "dist"),
+        publicPath: "dist/",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    }
+}
+
+module.exports = config;
